bf93c2080710bdc7ff5fb35eec0f7a52925877ef
geun02k <geun02k@gmail.com>
2025-05-22 22:37:09 +0900

    인터페이스 - 인터페이스에 의존하는 경우 구현체 변경

--- src/main/java/com/study/domain/springframeworkfirststep/chapter03/interfacesample/use/Call.java
+++ src/main/java/com/study/domain/springframeworkfirststep/chapter03/interfacesample/use/Call.java
@@ -1,7 +1,7 @@
 package com.study.domain.springframeworkfirststep.chapter03.interfacesample.use;
 
-import com.study.domain.springframeworkfirststep.chapter03.interfacesample.used.AddCalculator;
 import com.study.domain.springframeworkfirststep.chapter03.interfacesample.used.Calculator;
+import com.study.domain.springframeworkfirststep.chapter03.interfacesample.used.SubCalculator;
 
 public class Call {
     public static void main(String[] args) {
@@ -9,7 +9,7 @@ public class Call {
         /*
             자바의 다형성을 이용을 위해 인터페이스에 구현체 저장
          */
-        Calculator calculator = new AddCalculator(); // 인터페이스 구현클래스를 인스턴스화
+        Calculator calculator = new SubCalculator(); // 인터페이스 구현클래스를 인스턴스화
         Integer result = calculator.calc(10, 5);
 
         System.out.println("계산 결과는 ( " + result + " ) 입니다.");
