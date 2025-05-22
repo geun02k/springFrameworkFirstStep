1c361dcab730c61cf328f590c003267a07db20d4
geun02k <geun02k@gmail.com>
2025-05-22 22:38:26 +0900

    DI - DI를 이용하는 경우 구현체 변경

--- src/main/java/com/study/domain/springframeworkfirststep/chapter03/dependencyinjectionsample/used/MorningGreet.java
+++ src/main/java/com/study/domain/springframeworkfirststep/chapter03/dependencyinjectionsample/used/MorningGreet.java
@@ -1,12 +1,9 @@
 package com.study.domain.springframeworkfirststep.chapter03.dependencyinjectionsample.used;
 
-import org.springframework.stereotype.Component;
-
 /**
  * Greet 구현 클래스 <br>
  * 아침 인사 하기
  */
-@Component // 인스턴스 생성 어노테이션 (Greet 인터페이스의 객체로 MorningGreet 구현클래스 생성)
 public class MorningGreet implements Greet {
     @Override
     public void greeting() {
