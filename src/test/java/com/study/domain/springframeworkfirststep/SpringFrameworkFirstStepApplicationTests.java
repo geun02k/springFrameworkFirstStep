08821752a157be15048211f0d5e0736bad454ec7
geun02k <geun02k@gmail.com>
2025-05-22 20:36:22 +0900

    Initial commit

---
+++ src/test/java/com/study/domain/springframeworkfirststep/SpringFrameworkFirstStepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.domain.springframeworkfirststep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFrameworkFirstStepApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
