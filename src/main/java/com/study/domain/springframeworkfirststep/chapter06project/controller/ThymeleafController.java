1b006f1effbc8914fd60ce3209ada7cc4c356835
geun02k <geun02k@gmail.com>
2025-06-07 23:53:06 +0900

    타임리프 프로그램 만들기 - list, map 데이터 출력

--- src/main/java/com/study/domain/springframeworkfirststep/chapter06project/controller/ThymeleafController.java
+++ src/main/java/com/study/domain/springframeworkfirststep/chapter06project/controller/ThymeleafController.java
@@ -5,16 +5,40 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @Controller
 public class ThymeleafController {
 
     @GetMapping("show")
     public String showView(Model model) {
         Member member = new Member(1, "회원1");
+        Member member1 = new Member(10, "홍길동");
+        Member member2 = new Member(20, "이영희");
+
+        Map<String, Member> memberMap = new HashMap<String, Member>();
+        memberMap.put("hong", member1);
+        memberMap.put("lee", member2);
+
+        List<Member> memberList = new ArrayList<>();
+        memberList.add(member1);
+        memberList.add(member2);
+
+        List<String> directionList = new ArrayList<String>();
+        directionList.add("동");
+        directionList.add("서");
+        directionList.add("남");
+        directionList.add("북");
 
         // model에 데이터 추가
         model.addAttribute("name", "이순신");
         model.addAttribute("member", member);
+        model.addAttribute("directions", directionList);
+        model.addAttribute("memberMap", memberMap);
+        model.addAttribute("members", memberList);
 
         // view 이름 반환
         return "chapter06project/useThymeleaf";
