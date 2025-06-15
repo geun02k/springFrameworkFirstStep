b25ac38d36e6fd3a4419c73cb34aaeec103049d5
geun02k <geun02k@gmail.com>
2025-06-16 01:48:54 +0900

    Form에서 클릭한 버튼 식별하기 - Form의 name속성과 @RequestMapping의 params 속성 사용하기

--- src/main/java/com/study/domain/springframeworkfirststep/chapter07/controller/PathVariableController.java
+++ src/main/java/com/study/domain/springframeworkfirststep/chapter07/controller/PathVariableController.java
@@ -3,6 +3,7 @@ package com.study.domain.springframeworkfirststep.chapter07.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /** URL에 포함된 값 받기 */
@@ -36,5 +37,21 @@ public class PathVariableController {
         }
 
         return view;
+    }
+
+    /** 클릭한 버튼 판별 */
+    // @RequestMapping의 params 속성에 뷰의 버튼에 대응하는 name 속성 설정 시,
+    // 요청에서 어떤 버튼이 클릭되었는지 판별가능.
+    @PostMapping(value="send", params = "a")
+    public String showViewByButton1() {
+        return "chapter07/submitA";
+    }
+    @PostMapping(value="send", params = "b")
+    public String showViewByButton2() {
+        return "chapter07/submitB";
+    }
+    @PostMapping(value="send", params = "c")
+    public String showViewByButton3() {
+        return "chapter07/submitC";
     }
 }
